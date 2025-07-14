5f363cc8cbc2836981dc58d7c87e728714d182f6
PMisael <misaeljimenez961@gmail.com>
2025-07-13 23:50:26 -0600

    Creación de notebook para extraer frames_csv y ordenamiento de códigos

--- tests/ExtraeFrame.py
+++ tests/ExtraeFrame.py
@@ -2,16 +2,16 @@ import mediapipe as mp
 import cv2
 import pandas as pd
 
-
-VIDEO_PATH = 'Videos/otro_rotado_desplazado.mp4'
-CSV_PATH   = 'posicion_aleatoria.csv'
-LABEL      = 'Otra'
-
 class MpPose:
-    def __init__(self):
+    def __init__(self,video=False):
         self.mp_drawing=mp.solutions.drawing_utils
         self.mp_pose=mp.solutions.pose
-        self.model=self.mp_pose.Pose(model_complexity=2)
+        self.video=video
+        if self.video:
+            self.model=self.mp_pose.Pose(model_complexity=2)
+        else:
+            self.model=self.mp_pose.Pose(static_image_mode=True,model_complexity=2)
+            print("Analizando Imagen")
         self.results=None
     #
     def process(self,frame,draw=False):
@@ -33,7 +33,8 @@ class MpPose:
     def extrae_valores(self, label):
         row = [c for lm in self.results.pose_landmarks.landmark
                 for c in (lm.x, lm.y, lm.z)]
-        row.append(label)
+        if self.video:
+            row.append(label)
         return (row)
     #
     def create_lists(self):
@@ -50,22 +51,27 @@ class Video:
         self.frame = None
         self.data  = []
     # 
-    def process(self,label,csv_path,reproduce=False):
+    def process(self,label,csv_path,step=10,reproduce=False):
         cap=cv2.VideoCapture(self.path)
-        #print(columns)
+        contador=0
         #
         total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         print(f"Total de frames: {total_frames}")
-        while cap.isOpened():
-            status,self.frame=cap.read()
+        while cap.grab():
+            contador += 1
+            if contador < step:
+                continue
             #
+            status,self.frame=cap.read()
             if not status:
                 break
+            #
             self.pose.process(self.frame,True)
             puntos=self.pose.cuenta_puntos(0.1)
             #
             if puntos==33:
                 self.data.append(self.pose.extrae_valores(label))
+                contador = 0
             if reproduce:
                 cv2.putText(self.frame, f"Puntos: {puntos}" , (0, 80), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,0,255), 4)
                 cv2.imshow("frame",self.frame)
@@ -82,8 +88,28 @@ class Video:
             df=pd.DataFrame(self.data, columns=columns)
             df.to_csv(csv_path,index=False, float_format="%.6f")
             print(f'CSV guardado con {len(self.data)} fotogramas')
-
-
-if __name__=='__main__':
-    nuevo_video=Video(VIDEO_PATH)
-    nuevo_video.process(LABEL,CSV_PATH, True)
+#
+#
+class IMAGEN:
+    def __init__(self, path):
+        self.pose  = MpPose()
+        self.frame = None
+        self.path  = path
+    # 
+    def extrae(self,muestra=False,label="desconocido"):
+        self.frame=cv2.imread(self.path)
+        self.pose.process(self.frame,True)
+        puntos=self.pose.cuenta_puntos(0.1)
+        #
+        if muestra:
+            cv2.putText(self.frame, f"Puntos: {puntos}" , (0, 80), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,0,255), 4)
+            #
+            cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('frame', 800, 600) 
+            cv2.imshow("frame",self.frame)
+        #
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        if puntos==33:
+            return(self.pose.extrae_valores(label))
+        return(None)
